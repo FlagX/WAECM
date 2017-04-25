da377c7aad17a03e29890859e80f6e16d8a236ab
Stefan Griesler <e1125257@student.tuwien.ac.at>
2017-04-25 22:36:12 +0200

    display transactions on index

--- app/src/main/js/app.js
+++ app/src/main/js/app.js
@@ -33,6 +33,8 @@ class App extends React.Component {
     render() {
         return (
             <div>
+                <UserInfo/>
+                <Transactions/>
                 <GetCounterButton onGetCounter={this.onGetCounter}/>
                 <IncrementCounterButton onIncrementCounter={this.onIncrementCounter}/>
                 <b>Counter: {this.state.counter}</b>
@@ -107,7 +109,90 @@ class LogoutButton extends React.Component {
     }
 }
 
+class UserInfo extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {name: 'no_name',balance:0};
+    }
+    componentWillMount() {
+
+        client({method: 'GET', path: '/userinfo'}).done(response => {
+            this.setState({name: response.entity.username});
+            this.setState({balance: response.entity.balance});
+        });
+    }
+    render() {
+        return (
+            <ul>
+                <li>Username: {this.state.name}</li>
+                <li>Balance: {this.state.balance} €</li>
+            </ul>
+        )
+
+    }
+};
+
+class TransactionRows extends React.Component {
+    render() {
+        /* the ES6 version of const data = this.props.data */
+        const {data} = this.props;
+        /*
+         use map to perform the same function on
+         each element in the obj array
+         */
+
+        const row = data.map((data) =>
+            <tr>
+                <td>{data.id}</td>
+                <td>{data.description}</td>
+                <td>{data.value}</td>
+                <td>{data.owner.username}</td>
+                <td>{data.target.username}</td>
+                {/*<td>{data.created}</td>*/}
+                {/*<td>{data.commited}</td>*/}
+            </tr>
+        );
+        return (
+            <span>
+                <tr>
+                    <th>Id</th>
+                    <th>Description</th>
+                    <th>Value</th>
+                    <th>Owner</th>
+                    <th>Target</th>
+                </tr>
+                {row}</span>
+        );
+    }
+}
+
+class Transactions extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {transactions: []};
+    }
+    componentWillMount() {
+        client({method: 'GET', path: '/userinfo'}).done(response => {
+            this.setState({user: response.entity});
+            client({method: 'GET', path: '/transactionsByAccountId', entity: response}).done(response => {
+                this.setState({transactions: response.entity});
+            });
+        });
+
+    }
+    render() {
+        return (
+                <table>
+                    <caption>Transactions:</caption>
+                    <TransactionRows data={this.state.transactions} />
+                </table>
+        )
+    }
+}
+
 ReactDOM.render(
     <App />,
     document.getElementById('react')
 );
+
