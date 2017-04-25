08522641565d3e32db0e8264497ed6eedc380ebf
Stefan Griesler <e1125257@student.tuwien.ac.at>
2017-04-25 22:36:12 +0200

    display transactions on index

--- app/src/main/java/at/ac/tuwien/waecm/app/controller/TransactionController.java
+++ app/src/main/java/at/ac/tuwien/waecm/app/controller/TransactionController.java
@@ -1,5 +1,9 @@
 package at.ac.tuwien.waecm.app.controller;
 
+import at.ac.tuwien.waecm.app.persistence.repository.TransactionRepository;
+import at.ac.tuwien.waecm.common.persistence.dto.AccountDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,4 +27,9 @@ public class TransactionController {
         return true;
     }
 
+    @RequestMapping(method = RequestMethod.GET, path = "/transactionsByAccountId")
+    public String getTransactions(AccountDto account) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(transactionService.findAll());
+    }
 }
