9e57f69905bc4492ae5e68e35c851ec3edffa8bb
Stefan Griesler <e1125257@student.tuwien.ac.at>
2017-05-19 00:45:46 +0200

    Fix script

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-group6_ip="$(docker inspect bridge | jq '.[0].Containers[] | select(.Name=="group6") | .IPv4Address')"
-group6_ip=${group6_ip/\/16}
+group6_ip="$(docker inspect group6 | jq '.[].NetworkSettings.IPAddress')"
 docker run --network=abc -v /home/travis/.m2:/root/.m2 --name group6-test -e "group6_ip=$group6_ip" waecm-bsp1 test
