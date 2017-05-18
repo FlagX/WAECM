48edd8470cbe07d496970036da53ba1bb3d3b676
Stefan Griesler <e1125257@student.tuwien.ac.at>
2017-05-18 23:36:42 +0200

    fix ports for tests

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 group6_ip="$(cat test.txt | jq '.[0].Containers[] | select(.Name=="group6") | .IPv4Address')"
 group6_ip=${group6_ip/\/16}
-docker run --network=abc -v /home/travis/.m2:/root/.m2 -p 8080:8080 --name group6-test -e "group6_ip=$group6_ip" waecm-bsp1 test
+docker run --network=abc -v /home/travis/.m2:/root/.m2 --name group6-test -e "group6_ip=$group6_ip" waecm-bsp1 test
