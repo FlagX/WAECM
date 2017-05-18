5762aac72fa15f4c46fe816bcd7efb67f6d004cc
Stefan Griesler <e1125257@student.tuwien.ac.at>
2017-05-19 01:49:46 +0200

    inspect

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 group6_ip="$(docker inspect group6 | jq '.[].NetworkSettings.IPAddress')"
 chromedriver_ip="$(docker inspect chromedriver | jq '.[].NetworkSettings.IPAddress')"
-echo $group6_ip
-echo $chromedriver_ip
+echo docker inspect group6
+echo docker inspect chromedriver
 docker run --network=abc -v /home/travis/.m2:/root/.m2 --name group6-test -e "group6_ip=$group6_ip" -e "chromedriver_ip=$chromedriver_ip" waecm-bsp1 test
